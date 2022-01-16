e3853d1c64b79604c9bb0ce3059af390c402d5e5
Adônis Ramos <adonis.ramos@hotmail.com>
2022-01-16 14:45:46 -0300

    Criado atributos complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,19 +1,21 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar (self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Adônis')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Ramos'
-    print(p.nome)
-    print(p.idade)
-    p.idade = 33
-    print(p.idade)
+    elisa = Pessoa(nome='Elisa')
+    natercia = Pessoa(elisa, nome="Natercia")
+    print(Pessoa.cumprimentar(natercia))
+    print(id(natercia))
+    print(natercia.cumprimentar())
+    print(natercia.nome)
+    print(natercia.idade)
+
+    for filho in natercia.filhos:
+        print(filho.nome)
+
